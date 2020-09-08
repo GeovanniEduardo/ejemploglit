dfc7ae22433286b2ddd5291964af4a1148fc927e
Geovanni Macias <za17012336apopan.tecmm.edu.mx>
2020-09-08 18:26:38 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Mi nombre es {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Mi no. de control es 17012336')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Geovanni Eduardo López Macias')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
